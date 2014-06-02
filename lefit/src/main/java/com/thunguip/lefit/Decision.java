02a9b08489fcd5d75d384219f490d2e45c85a342
Noeljunior <liamgliam@gmail.com>
2014-06-02 11:21:05 +0100

    fix new notification and net popup acording to context

--- lefit/src/main/java/com/thunguip/lefit/Decision.java
+++ lefit/src/main/java/com/thunguip/lefit/Decision.java
@@ -30,11 +30,22 @@ public class Decision {
     public MessageParcel getMessageParcelByContext(int clickedcontext, long refer) {
         MessageParcel mp = new MessageParcel();
 
-        /* If there is no PersonStyle defined, then the popup is always the same */
-        if (preferences.getPersonStyle() == Preferences.PERSONSTYLE_NOTDEFINED) {
-            /* The person did not answer his first time */
-
+        /* Select which title */
+        if (isFirstTime()) { /* The first time */
             mp.title = 0;
+        }
+        else if (isClickedOnLvItem(clickedcontext)) { /* Clicked on specific LvItem */
+            mp.title = 3;
+        }
+        else if (isReferingToYesterday()) { /* It is about yesterday */
+            mp.title = 2;
+        }
+        else { /* It is about today */
+            mp.title = 1;
+        }
+
+        /* Select which phrase set, min, max and default */
+        if (whichProfile() == -1) { /* The first time */
             mp.phraseset = 0;
             mp.minphrase = 0;
 
@@ -44,80 +55,50 @@ public class Decision {
 
             mp.maxphrase = context.getResources().getStringArray(phraseid).length - 1;
             mp.defphrase = 1;
+        }
+        else { /* Not the first time, get the prase set based on formula */
+            if (whichProfile() == 0)
+                mp.phraseset = 1;
+            else
+                mp.phraseset = 2;
 
-            mp.showmessage = 0;
-
-            mp.referdate = Preferences.TimeHelper.getTodayDate();
-
-            return mp;
+            // TODO get min, max and default
         }
 
-        /* Select referDate and title*/
-        if (clickedcontext == CONTEXT_LVITEM) {
-            /* The person just clicked on an item, so */
-            mp.title = 3;
+        /* Select dailly message */
+        if (isFirstTime() || (preferences.isShowDaillyMessage() == false)) {
+            /* Do not show if it is the first time or if the user ask to hide those messages */
+            mp.showmessage = 0;
+        }
+        else { /* Else, show the messages */
+            mp.showmessage = 1;
+
+            // TODO get message set/subset based on how many days does the person answered
+            // TODO get message set/subset based on how many times did the person see the message
+        }
+
+        /* Select if this popup can be postponed or not */
+        if (!isClickedOnLvItem(clickedcontext) && canPostpone()) {
+            mp.showpostpone = 1;
+        }
+        else { /* It was clicked on LvItem or it is too closed to next day notification,
+                    else cannot be postponed */
+            mp.showpostpone = 0;
+        }
+
+        /* Get refer date */
+        if (isClickedOnLvItem(clickedcontext)) {
             mp.referdate = refer;
         }
-        else if (Preferences.TimeHelper.getTodayTime() < preferences.getNotificationTime()) {
-            /* It may be refering to the day before today */
+        else if (isReferingToYesterday()) {
             Calendar cal = Calendar.getInstance();
             cal.setTimeInMillis(Preferences.TimeHelper.getTodayDate());
             cal.add(Calendar.DAY_OF_MONTH, -1);
-
-            mp.title = 2;
             mp.referdate = cal.getTimeInMillis();
-
-            long gap = (preferences.getNotificationTime() - preferences.getNotificationCleanGap() - preferences.getPostponeDelay());
-
-            /*Log.d("Decision", "TIME: " + Preferences.TimeHelper.toString(preferences.getNotificationTime()) +
-                    "; GAP: " + Preferences.TimeHelper.toString(preferences.getNotificationCleanGap()) +
-                    "; PDELAY: " + Preferences.TimeHelper.toString(preferences.getPostponeDelay()) +
-                    "; SUM: " + Preferences.TimeHelper.toString(gap) +
-                    "; NOW: " + Preferences.TimeHelper.toString(Preferences.TimeHelper.getTodayTime()));*/
-
-            if (Preferences.TimeHelper.getTodayTime() < gap) {
-                mp.showpostpone = 1;
-            }
-            else {
-                mp.showpostpone = 0;
-            }
         }
         else {
-            /* It may be refering to today */
-            mp.title = 1;
             mp.referdate = Preferences.TimeHelper.getTodayDate();
-            mp.showpostpone = 1;
         }
-
-        /* Select the message set */
-        if (preferences.getPersonStyle() == Preferences.PERSONSTYLE_SEDENTARY ||
-                preferences.getPersonStyle() == Preferences.PERSONSTYLE_ACTIVE) {
-            /* Profile one */
-            mp.phraseset = 1;
-        }
-        else if (preferences.getPersonStyle() == Preferences.PERSONSTYLE_MODERATE ||
-                preferences.getPersonStyle() == Preferences.PERSONSTYLE_INTENSE) {
-            /* Profile two */
-            mp.phraseset = 2;
-        }
-
-        /* TODO Set the limits of phrases: by formula */
-        mp.minphrase = 1;
-        mp.maxphrase = 2;
-
-        /* Select the message set */
-        if (preferences.isShowDaillyMessage()) {
-            /* If the user wants to see dailly message */
-            mp.showmessage = 1;
-            mp.messageset = 0;
-            mp.defmessage = 0;
-
-            // TODO get which messagesubset to show: count how many time did the person answered
-            mp.messagesubset = 0;
-        }
-
-
-
 
         return mp;
     }
@@ -131,47 +112,74 @@ public class Decision {
         resultIntent.putExtra(MainService.SWITCH, MainService.NOTIFICATIONACTION);
         resultIntent.putExtra(MainService.NOTIFICATIONACTION, MainService.NOTIFCATION_OPNEN);
         PendingIntent resultPendingIntent = PendingIntent.getService(context, MainService.NOTIFID_PIRESULT, resultIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+        mBuilder.setContentIntent(resultPendingIntent);
 
-        /* Postpone Action */
-        Intent postponeIntent = new Intent(context, MainService.class);
-        postponeIntent.putExtra(MainService.SWITCH, MainService.NOTIFICATIONACTION);
-        postponeIntent.putExtra(MainService.NOTIFICATIONACTION, MainService.NOTIFCATION_POSTPONE);
-        PendingIntent postponePendingIntent = PendingIntent.getService(context, MainService.NOTIFID_PIPOSTPONE, postponeIntent, PendingIntent.FLAG_CANCEL_CURRENT);
 
-        /* Setting title and description */
-        int id;
-
-        if (preferences.getPersonStyle() == Preferences.PERSONSTYLE_NOTDEFINED) {
-            id = 0;
+        /* Select title and description */
+        int strid;
+        if (isFirstTime()) { /* The first time */
+            strid = 0;
         }
-        else if (Preferences.TimeHelper.getTodayTime() < preferences.getNotificationTime()) {
-            id = 2;
-
-            long gap = (preferences.getNotificationTime() - preferences.getNotificationCleanGap() - preferences.getPostponeDelay());
-
-            if (Preferences.TimeHelper.getTodayTime() < gap) {
-                mBuilder.addAction(R.drawable.ic_device_access_time, "Lembrar mais tarde", postponePendingIntent);
-            }
-
+        else if (isReferingToYesterday()) { /* It is about yesterday */
+            strid = 2;
         }
-        else {
-            id = 1;
-            mBuilder.addAction(R.drawable.ic_device_access_time, "Lembrar mais tarde", postponePendingIntent);
+        else { /* It is about today */
+            strid = 1;
+        }
+        mBuilder.setContentTitle(context.getResources().getStringArray(R.array.titles)[strid]);
+        mBuilder.setContentText(context.getResources().getStringArray(R.array.notifdesc)[strid]);
+
+        /* Select if can be postponed */
+        if (canPostpone()) {
+            Intent postponeIntent = new Intent(context, MainService.class);
+            postponeIntent.putExtra(MainService.SWITCH, MainService.NOTIFICATIONACTION);
+            postponeIntent.putExtra(MainService.NOTIFICATIONACTION, MainService.NOTIFCATION_POSTPONE);
+            PendingIntent postponePendingIntent = PendingIntent.getService(context, MainService.NOTIFID_PIPOSTPONE, postponeIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+            mBuilder.addAction(R.drawable.ic_device_access_time, context.getResources().getString(R.string.notifpostpone), postponePendingIntent);
         }
 
-
-        String title = context.getResources().getStringArray(R.array.titles)[id];
-        String description = context.getResources().getStringArray(R.array.notifdesc)[id];
-
+        /* Set the notifications user preferences */
         if (preferences.isNotificationVibrate()) {
             mBuilder.setSound(Uri.parse(preferences.getNotificationSound()));
             mBuilder.setDefaults(Notification.DEFAULT_VIBRATE | Notification.DEFAULT_LIGHTS);
         }
 
-        mBuilder.setContentTitle(title);
-        mBuilder.setContentText(description);
-        mBuilder.setContentIntent(resultPendingIntent);
+        /* Select if it is to update or to launch new */
 
         return mBuilder;
     }
+
+
+    public boolean isFirstTime() {
+        return preferences.getPersonStyle() == Preferences.PERSONSTYLE_NOTDEFINED;
+    }
+    public boolean isClickedOnLvItem(int clickedcontext) {
+        return clickedcontext == CONTEXT_LVITEM;
+    }
+    public boolean isReferingToYesterday() {
+        return Preferences.TimeHelper.getTodayTime() < preferences.getNotificationTime();
+    }
+    public boolean canPostpone() {
+        return Preferences.TimeHelper.getTodayTime() <
+                (preferences.getNotificationTime() - preferences.getNotificationCleanGap() - preferences.getPostponeDelay());
+    }
+    public int whichProfile() {
+        if (preferences.getPersonStyle() == Preferences.PERSONSTYLE_SEDENTARY ||
+                preferences.getPersonStyle() == Preferences.PERSONSTYLE_ACTIVE) {
+            /* Profile one */
+            return 0;
+        }
+        else if (preferences.getPersonStyle() == Preferences.PERSONSTYLE_MODERATE ||
+                preferences.getPersonStyle() == Preferences.PERSONSTYLE_INTENSE) {
+            /* Profile two */
+            return 1;
+        }
+        else if (preferences.getPersonStyle() == Preferences.PERSONSTYLE_NOTDEFINED) {
+            return -1;
+        }
+        return -1;
+    }
+
+
+
 }
